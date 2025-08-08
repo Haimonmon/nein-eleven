23d7283c337e84e2acf4c0fca15e549369a91adb
Haimonmon <beankor8@gmail.com>
2025-08-08 22:01:39 +0800

    Modified: renders tetris board with windows

--- pages/main_game.py
+++ pages/main_game.py
@@ -1,10 +1,17 @@
+import BitEngine
+
 class MainGame:
     """ Here lies the fun and gameplay """
     def __init__(self, main):
-        self.window = main.window
+        self.window: BitEngine.BitWindow = main.window
+        self.grid: BitEngine.BitGrid = BitEngine.BitGrid(columns = 25, cell_size = 30, display_grid = True, border_width = 3) 
+        
 
     def render(self) -> None:
-        self.window.render()
+        # self.window.background_color =  "#DFA05D"
+        self.window.add_object(self.grid)
+     
+
 
 if __name__ == "__main__":
       MainGame.render()
