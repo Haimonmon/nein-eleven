100a6961d06d76577a42dc97ca4877c5e5fdc5b5
Haimonmon <beankor8@gmail.com>
2025-08-08 15:23:49 +0800

    For grid or board interface

---
+++ bitEngine/ui/grid.py
@@ -0,0 +1,26 @@
+import pygame
+
+class BitGrid:
+    def __init__(self, height: int, width: int) -> None:
+        """ Tetris board """
+        self.__height = height
+        self.__width = width
+    
+
+    def render() -> None:
+        """ Renders a tetris board """
+        pass
+
+
+class BitGridScoreBoard:
+    def __init__(self, height: int, width: int) -> None:
+        """ Tetris board scoreboard """
+        self.__height = height
+        self.__width = width
+    
+    def render() -> None:
+        pass
+
+
+if __name__ == "__main__":
+      pass
