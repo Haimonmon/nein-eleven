ad993d1875b111cebe1c1459c067596221cc4115
Haimonmon <beankor8@gmail.com>
2025-08-08 21:56:52 +0800

    Modified: windows width

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pages
-import bitEngine as bit
+import BitEngine
 
 class Tetra:
     """ Welcome to Tetra game """
     def __init__(self):
-        self.window = bit.BitWindow()
+        self.engine = BitEngine
+        self.window = self.engine.BitWindow(740, width = 1200)
         self.main_game = pages.MainGame(self)
 
     def start(self) -> None:
+        self.main_game.render()
         self.window.render()
-
+       
+        
 
 if __name__ == "__main__":
     tetris = Tetra()
