1157e21d408f3ba11a44ab10f5a751c50ab0b392
Haimonmon <beankor8@gmail.com>
2025-08-15 20:05:45 +0800

    Modified: Class name engine

--- bitEngine/engine.py
+++ bitEngine/engine.py
@@ -10,12 +10,12 @@ class BruhTheresNoWindows(Exception):
         super().__init__(message)
 
 
-class BitEngine:
+class Bit:
     """ The Bit's mighty Assembler """
     def __init__(self):
         self.window = None
 
-    
+
     def create_window(self) -> None:
          """ Creates a tetris window """
          self.window = BitInterfaceWindow(height = 740, width = 1200)
@@ -27,9 +27,9 @@ class BitEngine:
              raise BruhTheresNoWindows()
         
         grid_logic = self.window.add_object(BitLogicGrid(columns = 25))
-        grid_spawner_logic = self.window.add_object(BitLogicTetrominoGridSpawner(self.window, grid_logic)) 
-
-        ui_grid = self.window.add_object(BitInterfaceGrid(grid_logic, display_grid = True))
+        self.window.add_object(BitLogicTetrominoGridSpawner(self.window, grid_logic)) 
+        
+        self.window.add_object(BitInterfaceGrid(grid_logic, display_grid = True))
 
 
     def play(self) -> None:
@@ -37,7 +37,7 @@ class BitEngine:
 
 
 if __name__ == "__main__":
-        engine = BitEngine()
+        engine = Bit()
         engine.create_window()
         engine.create_grid()
         engine.play()
