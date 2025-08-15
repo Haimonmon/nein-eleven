125721c68595b6bf961af3f5131d08131fca6b44
Haimonmon <beankor8@gmail.com>
2025-08-15 17:50:23 +0800

    Modified: added parameters

--- bitEngine/core/core_grid.py
+++ bitEngine/core/core_grid.py
@@ -1,3 +1,5 @@
+import random
+
 from typing import Literal, Dict, List, Tuple, Type
 
 from .core_tetromino import BitLogicTetromino
@@ -5,7 +7,7 @@ from BitEngine.ui import BitInterfaceTetromino
 
 class BitLogicGrid:
     """ Collision logics """
-    def __init__(self, rows: int = 20, columns: int = 30):
+    def __init__(self, rows: int = 20, columns: int = 30) -> None:
         self.rows = rows
         self.columns = columns
 
@@ -17,57 +19,65 @@ class BitLogicGrid:
 
 
 class BitLogicTetrominoGridSpawner:
+    """ Basically just a spawner 🤓☝️"""
     def __init__(self, window, grid_logic: BitLogicGrid):
-         self.window = window 
+        self.window = window 
 
-         self.grid_logic = grid_logic
-         self.tetromino_logic = BitLogicTetromino
-         self.tetromino_interface = BitInterfaceTetromino
+        self.grid_logic = grid_logic
+        self.tetromino_logic = BitLogicTetromino
+        self.tetromino_interface = BitInterfaceTetromino
 
-         self.spawn_test = True
+        self.spawn_test = True
+
+        self.piece: Dict[str, List[Tuple[int, int]]] = {
+            "O": [(0, 0), (1, 0), (0, 1), (1, 1)],
+            "I": [(0, 0), (1, 0), (2, 0), (3, 0)]
+        }
 
 
     def update(self) -> None:
         # * Spawn once only for testing
         if self.spawn_test:
-            self.spawn(piece_shape = "O")
+            self.spawn(piece_shape = "i")
             self.spawn_test = False
 
 
     def spawn(self, piece_shape: Literal["O", "I"] = "O") -> None:
         """ spawns tetromino pieces on the grid """
-        # * Pick starting location
-        start_x = (self.grid_logic.columns // 2) - 2
+        
+        piece_shape = piece_shape.upper()
+        # * I want the tetromino to spawn within in any area of the spawn 🫡
+        start_x = random.randint(0, self.grid_logic.columns)
         start_y = 0
 
-        shape_coords = self.create(piece_shape)
+        created_tetromino: BitLogicTetromino = self.create(piece_shape)
 
-        positioned_shape = [(x + start_x, y + start_y)
-                            for x, y in shape_coords]
+        # * Solution for over exceeding of tetromino because of randint
+        if start_x + created_tetromino.width >= self.grid_logic.columns:
+            start_x = self.grid_logic.columns - created_tetromino.width - 1
 
-        self.tetromino_logic.shape = positioned_shape
-
-        for x, y in positioned_shape:
+        # * Change tetromino coordinates base on grid
+        tetromino_coordinates = [(x + start_x, y + start_y) for x, y in created_tetromino.coordinates]
+        
+        # * Position Tetromino on the grid
+        for x, y in tetromino_coordinates:
             self.grid_logic.cell_coordinates[y][x] = 1
 
+        created_tetromino.coordinates = tetromino_coordinates
+        
 
-    def create(self, piece_shape: Literal["0", "I"]) -> None:
+    def create(self, piece_shape: Literal["0", "I"]) -> BitLogicTetromino:
         """ Creates the tetromino peice coordinates or its piece shape """
 
-        piece: Dict[str, List[Tuple[int, int]]] = {
-            "O": [(5, 0), (6, 0), (5, 1), (6, 1)],
-            "I": [(3, 0), (4, 0), (5, 0), (6, 0)]
-        }
+        coordinates = self.piece.get(piece_shape)
 
-        created_logic_tetromino: BitLogicTetromino = self.tetromino_logic(self.grid_logic)
+        tetromino_width = max(x for x, _ in coordinates)
+        tetromino_height = max(y for _, y in coordinates)
 
-        # * SET STATES 🟢🍏
-        created_logic_tetromino.word = "TESTING"
-        created_logic_tetromino.coordinates = piece.get(piece_shape)
-        created_logic_tetromino.piece_word = piece_shape
+        created_logic_tetromino: BitLogicTetromino = self.tetromino_logic(self.grid_logic, tetromino_width, tetromino_height, piece_shape, coordinates)
 
         # * ADDS TO THE WINDOW SURFACE
         self.window.add_object(created_logic_tetromino)
         self.window.add_object(self.tetromino_interface(created_logic_tetromino))
 
-        return created_logic_tetromino.coordinates
+        return created_logic_tetromino
