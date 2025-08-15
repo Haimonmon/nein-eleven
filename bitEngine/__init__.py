7727cbcf0bafb9e59f46bb4bb064f7afa40a1005
Haimonmon <beankor8@gmail.com>
2025-08-15 20:00:01 +0800

    Modified: BitEngine to Bit

--- bitEngine/__init__.py
+++ bitEngine/__init__.py
@@ -2,13 +2,12 @@
 ## Bit Engine
 Tetra game engine built in `pygame`
 """
-from .engine import BitEngine
+from .engine import Bit
 
 from .ui import BitInterfaceWindow, BitInterfaceGrid, BitInterfaceGridScoreBoard, BitInterfaceTetromino
 from .utils import BitPickleFileManager, BitFileManager
 
 __all__ = [
-    "BitEngine",
     "BitInterfaceWindow", "BitInterfaceGrid", "BitInterfaceGridScoreBoard", "BitInterfaceTetromino",
     "BitFileManager", "BitPickleFileManager"
 ]
