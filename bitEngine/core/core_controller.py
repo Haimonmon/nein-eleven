f626e05728771aea8ed10f35c8dc79457f7136cd
Haimonmon <beankor8@gmail.com>
2025-08-16 12:42:20 +0800

    Added: Object or tetromino controller

---
+++ bitEngine/core/core_controller.py
@@ -0,0 +1,10 @@
+class BitLogicController:
+    """ The Main Controller of Tetromino's """
+    def __init__(self) -> None:
+        pass
+
+    def update() -> None:
+        pass
+
+if __name__ == "__main__":
+      pass
