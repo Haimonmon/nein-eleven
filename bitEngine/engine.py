c7884d2e285db2742607ff5c9cdb0624a3975120
Haimonmon <beankor8@gmail.com>
2025-08-19 06:59:51 +0800

    modified: create_window now returns instance of class BitInterfaceWindow

--- bitEngine/engine.py
+++ bitEngine/engine.py
@@ -2,7 +2,7 @@ from .core import *
 from .ui import *
 from .utils import *
 
-from typing import Type
+from typing import Type, Tuple
 
 class BruhTheresNoWindows(Exception):
     """ Raised when the window object is missing. """
@@ -17,12 +17,14 @@ class Bit:
         self.tick_speed = tick_speed * 100
 
 
-    def create_window(self) -> None:
+    def create_window(self, height: int = 740, width: int = 1200, title: str  = "The Bit Engine", icon: str = "favicon.png", background_color: str | Tuple[int] = "#1A1A1A") -> BitInterfaceWindow:
          """ Creates a tetris window """
-         self.window = BitInterfaceWindow(height = 740, width = 1200)
+         self.window = BitInterfaceWindow(height, width, title, icon, background_color)
+
+         return self.window
 
 
-    def create_grid(self, rows: int = 20, columns: int = 30, display_grid: bool = True) -> None:
+    def create_grid(self, rows: int = 20, columns: int = 30, cell_size: int = 30, display_grid: bool = True, border_color: str | Tuple[int] = (100, 100, 100), border_width: int = 1) -> None:
         """ Creates a tetris board or grid """
         if not self.window:
              raise BruhTheresNoWindows()
@@ -30,7 +32,7 @@ class Bit:
         grid_logic = self.window.add_object(BitLogicGrid(rows, columns))
         self.window.add_object(BitLogicTetrominoGridSpawner(self.window, grid_logic, self.tick_speed)) 
 
-        self.window.add_object(BitInterfaceGrid(grid_logic, display_grid = display_grid))
+        self.window.add_object(BitInterfaceGrid(grid_logic, cell_size, display_grid, border_color, border_width))
 
 
     def play(self) -> None:
