479668744b27d558020b7bbf24ad54ffef0792e6
Haimonmon <beankor8@gmail.com>
2025-08-19 07:01:01 +0800

    modified: grid creaton change to method

--- pages/main_game.py
+++ pages/main_game.py
@@ -1,15 +1,17 @@
-import BitEngine
+import bitEngine
 
 class MainGame:
     """ Here lies the fun and gameplay """
     def __init__(self, main):
-        self.window: BitEngine.BitWindow = main.window
-        self.grid: BitEngine.BitGrid = BitEngine.BitGrid(columns = 25, cell_size = 30, display_grid = True, border_width = 3) 
+        self.main = main
+        self.window: bitEngine.BitInterfaceWindow = main.window
+        self.grid = self.main.engine.create_grid()
+        # self.grid: bitEngine.BitInterfaceGrid = bitEngine.BitInterfaceGrid(columns = 25, cell_size = 30, display_grid = True, border_width = 3) 
         
 
     def render(self) -> None:
         # self.window.background_color =  "#DFA05D"
-        self.window.add_object(self.grid)
+        self.main.engine.play()
      
 
 
