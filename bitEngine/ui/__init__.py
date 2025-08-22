2ad6c1a773e9e7c52f0bbf6d22983f57d6d676dd
Haimonmon <beankor8@gmail.com>
2025-08-22 21:08:32 +0800

    Added: Next piece interface are on work

--- bitEngine/ui/__init__.py
+++ bitEngine/ui/__init__.py
@@ -1,9 +1,11 @@
 from .ui_window import BitInterfaceWindow
 from .ui_grid import BitInterfaceGrid, BitInterfaceGridScoreBoard
 from .ui_tetromino import BitInterfaceTetromino
+from .ui_next_piece_view import BitInterfaceNextPieceView
 
 __all__ = [
     "BitInterfaceWindow",
     "BitInterfaceGrid", "BitInterfaceGridScoreBoard",
-    "BitInterfaceTetromino"
+    "BitInterfaceTetromino",
+    "BitInterfaceNextPieceView"
 ]
