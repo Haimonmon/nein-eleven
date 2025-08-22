6dfe9b01ceb43a68418328ca203a1f37559d60af
Haimonmon <beankor8@gmail.com>
2025-08-23 04:25:28 +0800

    Added: scoreboard logic import

--- bitEngine/core/__init__.py
+++ bitEngine/core/__init__.py
@@ -5,12 +5,14 @@ Contains bit engine's logic
 
 from .core_tetromino import BitLogicTetromino
 from .core_controller import BitLogicController
+from .core_scoreboard import BitLogicScoreboard
 from .core_next_piece_view import BitLogicNextPiece
 from .core_grid import BitLogicGrid, BitLogicTetrominoGridSpawner, BitLogicLineCleaner
 
 __all__ = [
     "BitLogicTetromino",
     "BitLogicController",
+    "BitLogicScoreboard",
     "BitLogicNextPiece",
     "BitLogicGrid", "BitLogicTetrominoGridSpawner", "BitLogicLineCleaner"
 ]
