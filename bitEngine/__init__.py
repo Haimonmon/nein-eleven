a4da0d955464145acff1f5ade2e71064a4d6ca95
Haimonmon <beankor8@gmail.com>
2025-08-23 04:25:07 +0800

    Removed: old scoreboard

--- bitEngine/__init__.py
+++ bitEngine/__init__.py
@@ -4,10 +4,10 @@ Tetra game engine built in `pygame`
 """
 from .engine import Bit
 
-from .ui import BitInterfaceWindow, BitInterfaceGrid, BitInterfaceGridScoreBoard, BitInterfaceTetromino
+from .ui import BitInterfaceWindow, BitInterfaceGrid, BitInterfaceTetromino
 from .utils import BitPickleFileManager, BitFileManager
 
 __all__ = [
-    "BitInterfaceWindow", "BitInterfaceGrid", "BitInterfaceGridScoreBoard", "BitInterfaceTetromino",
+    "BitInterfaceWindow", "BitInterfaceGrid", "BitInterfaceTetromino",
     "BitFileManager", "BitPickleFileManager"
 ]
