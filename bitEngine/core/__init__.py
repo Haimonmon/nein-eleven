a23f83184628db2a9dbef52852250ac789375b1a
Haimonmon <beankor8@gmail.com>
2025-08-22 21:04:05 +0800

    Added: Next piece logic

--- bitEngine/core/__init__.py
+++ bitEngine/core/__init__.py
@@ -5,10 +5,12 @@ Contains bit engine's logic
 
 from .core_tetromino import BitLogicTetromino
 from .core_controller import BitLogicController
+from .core_next_piece_view import BitLogicNextPiece
 from .core_grid import BitLogicGrid, BitLogicTetrominoGridSpawner, BitLogicLineCleaner
 
 __all__ = [
     "BitLogicTetromino",
     "BitLogicController",
+    "BitLogicNextPiece",
     "BitLogicGrid", "BitLogicTetrominoGridSpawner", "BitLogicLineCleaner"
 ]
