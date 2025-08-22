8b6bb0e4b6f56ce33df72604377b180c0a83b4dd
Haimonmon <beankor8@gmail.com>
2025-08-23 04:27:13 +0800

    Added: relativ eimport for scoreboard interface

--- bitEngine/ui/__init__.py
+++ bitEngine/ui/__init__.py
@@ -1,11 +1,13 @@
 from .ui_window import BitInterfaceWindow
-from .ui_grid import BitInterfaceGrid, BitInterfaceGridScoreBoard
+from .ui_grid import BitInterfaceGrid
 from .ui_tetromino import BitInterfaceTetromino
 from .ui_next_piece_view import BitInterfaceNextPieceView
+from .ui_scoreboard import BitInterfaceScoreBoard
 
 __all__ = [
     "BitInterfaceWindow",
-    "BitInterfaceGrid", "BitInterfaceGridScoreBoard",
+    "BitInterfaceGrid",
     "BitInterfaceTetromino",
-    "BitInterfaceNextPieceView"
+    "BitInterfaceNextPieceView",
+    "BitInterfaceScoreBoard"
 ]
