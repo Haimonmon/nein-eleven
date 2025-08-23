6ae78369580def7b0401650e82168268b696999a
Haimonmon <beankor8@gmail.com>
2025-08-23 10:58:02 +0800

    Added: page handler in action

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ class Tetra:
     """ Welcome to Tetra game """
     def __init__(self):
         self.engine = bitEngine.Bit()
-        self.window = self.engine.create_window(crt_effect = True)
-        self.main_game = pages.MainGame(self)
+
+        self.created_window = self.engine.create_window(crt_effect = True)
+        self.window = self.created_window["window"]
+        self.window_page_manager = self.created_window["window_page_manager"]
+
+        self.main_menu = self.window_page_manager.add_page("main_menu", pages.MainMeny)
+        self.main_game = self.window_page_manager.add_page("main_game", pages.MainGame)
+
 
     def start(self) -> None:
-        self.main_game.render()
-        
+        self.window_page_manager.set_page(self.main_menu, self)
+        self.engine.play()
+
+
 if __name__ == "__main__":
     tetris = Tetra()
     tetris.start()
