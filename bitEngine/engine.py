415b28138574f58c777bede1ffbdf8c228bd7df3
Haimonmon <beankor8@gmail.com>
2025-08-23 10:56:49 +0800

    Added: PageManager instance added on window creation as return

--- bitEngine/engine.py
+++ bitEngine/engine.py
@@ -32,12 +32,16 @@ class Bit:
 
         self.num_controllers = 0
 
-    def create_window(self, height: int = 740, width: int = 1200, title: str = "The Bit Engine", icon: str = "favicon.png", background_color: str | Tuple[int] = "#1A1A1A", scanline_alpha: int = 70, bend_amount: float = 0.85, flicker_intensity: int = 20, crt_effect: bool = False) -> BitInterfaceWindow:
+
+    def create_window(self, height: int = 740, width: int = 1200, title: str = "The Bit Engine", icon: str = "favicon.png", background_color: str | Tuple[int] = "#1A1A1A", scanline_alpha: int = 70, bend_amount: float = 0.85, flicker_intensity: int = 20, crt_effect: bool = False) -> Dict[str, object]:
         """ Creates a tetris window """
 
         self.window = BitInterfaceWindow(height, width, title, icon, background_color, scanline_alpha, bend_amount, flicker_intensity, crt_effect)
 
-        return self.window
+        return {
+            "window": self.window,
+            "window_page_manager": self.window.page_manager
+        }
 
 
     @require_window
