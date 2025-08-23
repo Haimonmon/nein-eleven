e86bce8eae12b62abbb0057620ce854378c89da7
Haimonmon <beankor8@gmail.com>
2025-08-23 10:58:46 +0800

    Added: required methods for the page handler

--- pages/main_game.py
+++ pages/main_game.py
@@ -1,35 +1,17 @@
 import bitEngine
 
+from typing import Literal
+
 class MainGame:
     """ Here lies the fun and gameplay """
-    def __init__(self, main):
+
+    def __init__(self, main, picked_gamemode: Literal["SinglePlayer", "PlayerVsPlayer", "PlayerVsAi"] = None):
         self.main = main
         self.engine: bitEngine.Bit = main.engine
 
         self.window = main.window
-        self.piece_viewer = self.engine.create_piece_viewer(width = 170, height = 300, position_x = 670, position_y = 370, num_piece_display = 2, border_color = "gold")
-        self.grid = self.engine.create_grid(piece_view = self.piece_viewer["piece_view_logic"], columns=10, rows=20, position_y = 70, position_x = 300, display_grid = False, border_color = "blue")
-        self.scoreboard = self.engine.create_scoreboard(self.grid["grid_line_cleaner"], width = 170, height = 150, position_x = 670, position_y = 70, border_color = "gold")
-        self.controller = self.engine.add_controller(self.grid)
-        
 
-    def render(self) -> None:
-        # tetromino_colors =  [
-        #     "#FFD700",  # gold
-        #     "#FFB700",  # rich gold
-        #     "#FFA500",  # orange-gold
-        #     "#FFF8DC",  # cornsilk (soft gold highlight)
-        #     "#F5DEB3",  # wheat (warm golden tone)
-        #     "#FFFACD",  # lemon chiffon (light reflection)
-        #     "#FFDAB9",  # peach puff (soft highlight)
-        #     "#FFF5E1",  # champagne glow
-        #     "#E6BE8A",  # antique gold
-        #     "#B8860B",  # dark goldenrod (deep gold shadow)
-        #     "#DAA520",  # goldenrod
-        #     "#FFFAF0"   # floral white (flash reflection)
-        # ]
-
-        tetromino_colors = [
+        self.tetromino_colors = [
             "#FF0000",  # Neon Red
             "#FF7F00",  # Orange
             "#FFFF00",  # Bright Yellow
@@ -43,11 +25,48 @@ class MainGame:
             "#FFD700",  # Gold (classic arcade shine)
             "#FF69B4"   # Hot Pink (extra pop)
         ]
+        
+        self.picked_gamemode = picked_gamemode
 
+        self.gamemodes = {
+            "SinglePlayer": self.create_single_player_match,
+            "PlayerVsPlayer": self.create_single_player_match,
+            "PlayerVsAi": self.create_single_player_match
+        }
+
+        if self.picked_gamemode in self.gamemodes:
+            self.gamemodes.get(self.picked_gamemode)()
+
+
+    def render(self, surface) -> None:
+        """ Display's the main game """
+        print(f"Rendering {self.picked_gamemode} . . .")
+
+        
+
+
+    def update(self) -> None:
+        pass
+
+
+    def control(self, events) -> None:
+        pass
+
+
+    def create_single_player_match(self) -> None:
+        """ Creates the single player match """
+        print("Creating .. ")
+
+        self.piece_viewer = self.engine.create_piece_viewer(width = 170, height = 300, position_x = 670, position_y = 370, num_piece_display = 2, border_color = "gold")
+        self.grid = self.engine.create_grid(piece_view = self.piece_viewer["piece_view_logic"], columns=10, rows=20, position_y = 70, position_x = 300, display_grid = False, border_color = "blue")
+        self.scoreboard = self.engine.create_scoreboard(self.grid["grid_line_cleaner"], width = 170, height = 150, position_x = 670, position_y = 70, border_color = "gold")
+        self.controller = self.engine.add_controller(self.grid)
 
         self.window.background_color =  "black"
-        self.grid["grid_spawner"].change_tetromino_appearance(tetromino_colors, (0,0,0), "blue")
-        self.engine.play()
+        self.grid["grid_spawner"].change_tetromino_appearance(self.tetromino_colors, (0,0,0), "blue")
+
+    
+  
         
      
 
